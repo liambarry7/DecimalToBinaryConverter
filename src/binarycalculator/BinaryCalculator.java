f964a56da724627648f3817b407c927ed36d8478
LB7 <liambarry406@yahoo.co.uk>
2023-09-29 11:41:28 +0100

    test

--- src/binarycalculator/BinaryCalculator.java
+++ src/binarycalculator/BinaryCalculator.java
@@ -7,13 +7,14 @@ public class BinaryCalculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int[] conversion = new int[8];
+
         
         System.out.println("Enter Denary number");
         int userNumber = input.nextInt();
         
         for (int i = 0; i < 8; i++) {
             int binaryNumber = userNumber % 2;
-            System.out.println(binaryNumber);
+            //System.out.println(binaryNumber);
             userNumber = userNumber / 2;
             conversion[i] = binaryNumber;
         }
